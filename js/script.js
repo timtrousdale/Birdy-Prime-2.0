59a358d06226c3fafae1d2e0d0135ed2db54579f
theLephty <theLephty@gmail.com>
2015-10-20 10:12:02 -0600

    objects
    added css id to data object

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 /*globals angular*/
 
 var app = angular.module('birdApp', []);
-app.controller('birdCtrl', function () {
+app.controller('birdController', function () {
     var vm = this;
-    vm.test = "Birds";
-
 
     function createArray(length) {
         var arr = new Array(length || 0),
@@ -26,8 +24,33 @@ app.controller('birdCtrl', function () {
 
     for (var i = 0; i < vm.aBirds.length; i++) {
         for (var j = 0; j < vm.aBirds[i].length; j++) {
-            vm.aBirds[i][j] = Math.round((Math.random() - nChanceReduction));
+            vm.aBirds[i][j] = {
+                value: Math.round((Math.random() - nChanceReduction))
+            };
         }
     }
+
+    vm.data = {
+        id: [{
+            value: "row1",
+            row: this.aBirds[0]
+        }, {
+            value: "row2",
+            row: this.aBirds[1]
+        }, {
+            value: "row3",
+            row: this.aBirds[2]
+        }],
+    };
+
+    /*    vm.aBirds.unshift({
+            value: "row3"
+        });
+        vm.aBirds.unshift({
+            value: "row2"
+        });
+        vm.aBirds.unshift({
+            value: "row1"
+        });*/
     console.log(vm.aBirds);
 });
